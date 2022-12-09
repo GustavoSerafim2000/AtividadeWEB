8617ac6642739a49b9a20269425b71bcea24dec7
GustavoSerafim2000 <120136442+GustavoSerafim2000@users.noreply.github.com>
2022-12-08 21:25:16 -0300

    Delete Atividade_GustavoSerafim directory

--- Atividade_GustavoSerafim/aula.js
+++
@@ -1,8 +0,0 @@
-import { dataLoad } from "./students/index.js";
-import { loadEvents } from "./events/index.js";
-
-$(function() {
-    dataLoad();
-    loadEvents();
-});
-
--- Atividade_GustavoSerafim/students/showAllStudentsResult.js
+++
@@ -1,8 +0,0 @@
-import { getAllStudents } from "./index.js";
-
-function showAllStudentsResult() {
-    const allStudents = getAllStudents();
-    $("#result").html(JSON.stringify(allStudents));
-}
-
-export { showAllStudentsResult }
